836506e8e0b8b1638d57baff37da433b1e51951a
turulomio <turulomio@ecefa015-bacb-442a-bd82-e7c1c7b1ecc8>
2008-07-14 10:12:54 +0000

    Nuevo versión con pyQt

---
+++ pyglParchis/glparchis.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*- 
+
+import sys
+sys.path.append("ui")
+sys.path.append("images")
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from frmMain import *
+
+def help():
+    print "Ayuda"
+
+#Creamos la aplicacion principal y conectamos la señal lastWindowClosed()
+#(ultima ventana cerrada) con la funcion quit() (salir de la aplicacion)
+app = QApplication(sys.argv)
+
+translator = QTranslator(app)
+locale=QLocale()
+a=locale.system()
+translator.load("glparchis_" + a.name() + ".qm")
+app.installTranslator(translator);
+
+frmMain = frmMain() 
+frmMain.show()
+
+sys.exit(app.exec_())
+
