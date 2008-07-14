d320f851daaefc3880c0a8ce44f4f24adeb77059
turulomio <turulomio@ecefa015-bacb-442a-bd82-e7c1c7b1ecc8>
2008-07-14 10:12:54 +0000

    Nuevo versión con pyQt

---
+++ glparchis_fr.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="glparchis.py" line="21"/>
+        <source>mierda</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>frmMain</name>
+    <message>
+        <location filename="ui/Ui_frmMain.py" line="57"/>
+        <source>glParchis 2008</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Ui_frmMain.py" line="58"/>
+        <source>A&amp;yuda</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Ui_frmMain.py" line="59"/>
+        <source>J&amp;ugar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Ui_frmMain.py" line="60"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Ui_frmMain.py" line="61"/>
+        <source>S&amp;alir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Ui_frmMain.py" line="62"/>
+        <source>Acerca de</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/Ui_frmMain.py" line="63"/>
+        <source>Jugar</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>wdgUserPanel</name>
+    <message>
+        <location filename="ui/Ui_wdgUserPanel.py" line="62"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
