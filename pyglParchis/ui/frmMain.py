5f339b2cfe4400e4558326219747eff6bab5faa4
turulomio <turulomio@ecefa015-bacb-442a-bd82-e7c1c7b1ecc8>
2010-05-24 10:57:33 +0000

    Se va mejorando pero falta mucho

--- pyglParchis/ui/frmMain.py
+++ pyglParchis/ui/frmMain.py
@@ -1,5 +1,5 @@
 ## -*- coding: utf-8 -*-
-import sys
+import sys,  random
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
@@ -33,13 +33,25 @@ class frmMain(QMainWindow, Ui_frmMain):#
         p4=wdgUserPanel(self.panel4)
         p4.lblAvatar.setPixmap(QtGui.QPixmap(":/glparchis/keka.png"))
         p4.show()
-#        self.lstLog.addColumn("Log")
-#        self.lstLog.setRootIsDecorated(1)
         self.logs = []
+        self.dado1 = QtGui.QIcon()
+        self.dado1.addPixmap(QtGui.QPixmap(":/glparchis/cube1.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.dado2 = QtGui.QIcon()
+        self.dado2.addPixmap(QtGui.QPixmap(":/glparchis/cube2.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.dado3 = QtGui.QIcon()
+        self.dado3.addPixmap(QtGui.QPixmap(":/glparchis/cube3.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.dado4 = QtGui.QIcon()
+        self.dado4.addPixmap(QtGui.QPixmap(":/glparchis/cube4.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.dado5= QtGui.QIcon()
+        self.dado5.addPixmap(QtGui.QPixmap(":/glparchis/cube5.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.dado6 = QtGui.QIcon()
+        self.dado6.addPixmap(QtGui.QPixmap(":/glparchis/cube6.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         QtCore.QObject.connect(self.ogl, QtCore.SIGNAL('newLog(QString)'), self.lstLog_newLog)  
+        QtCore.QObject.connect(self.ogl, QtCore.SIGNAL('cambiar_jugador()'), self.cambiar_jugador)  
+        QtCore.QObject.connect(self.ogl, QtCore.SIGNAL('volver_a_tirar()'), self.volver_a_tirar)  
     
     def lstLog_newLog(self, log):
-        self.logs.append(log)
+        self.logs.insert(0, str(self.ogl.jugadoractual) + "|" + log)
         self.lstLog.setModel(QStringListModel(QStringList(self.logs)))
         self.lstLog.show()
 
@@ -53,11 +65,71 @@ class frmMain(QMainWindow, Ui_frmMain):#
         sys.exit()
     
     @QtCore.pyqtSlot()      
-    def on_actionJugar_activated(self):
-        print "Ha salido un 3"
-        self.ogl.dado=3
+    def on_actionDado_activated(self):        
+        def jugador_tiene_todas_fichas_en_casa():
+            for f in self.ogl.fichas:
+                if f.jugador==self.ogl.jugadoractual:
+                    if f.ruta!=0:
+                        return False
+            return True
+        numero= int(random.random()*6)+1
+        if numero==1:
+            self.actionDado.setIcon(self.dado1)    
+        elif numero==2:
+            self.actionDado.setIcon(self.dado2)    
+        elif numero==3:
+            self.actionDado.setIcon(self.dado3)    
+        elif numero==4:
+            self.actionDado.setIcon(self.dado4)    
+        elif numero==5:
+            self.actionDado.setIcon(self.dado5)    
+        elif numero==6:
+            self.actionDado.setIcon(self.dado6)        
+        
+        #LOGICA QUE NO REQUIERE LA INTEVENCION DEL USUARIO
+        if numero!=5 and jugador_tiene_todas_fichas_en_casa()==True:
+            self.lstLog_newLog("No ha salido un 5 y las tienes todas en casa")
+            self.cambiar_jugador()
+            return
+        
+        self.lstLog_newLog("Ha salido un " + str(numero))        
+        self.ogl.dado=numero
+        self.ogl.pendiente=1
+        self.ogl.historicodado.insert(0, numero)
+        self.actionDado.setEnabled(False)
+
+                    
+    def cambiar_jugador(self):
+        if self.ogl.jugadoractual==0:
+            self.panel1.setEnabled(False)
+        elif self.ogl.jugadoractual==1:
+            self.panel2.setEnabled(False)
+        elif self.ogl.jugadoractual==2:
+            self.panel3.setEnabled(False)
+        elif self.ogl.jugadoractual==3:
+            self.panel4.setEnabled(False)
+            
+        self.lstLog_newLog("cambiando a jugador "  + str(self.ogl.jugadoractual))
+        self.ogl.jugadoractual=self.ogl.jugadoractual+1
+        self.ogl.historicodado=[]
+        self.ogl.pendiente=2
+        if self.ogl.jugadoractual>=4:
+            self.ogl.jugadoractual=0
+        self.actionDado.setEnabled(True)       
+        
+        if self.ogl.jugadoractual==0:
+            self.panel1.setEnabled(True)
+        elif self.ogl.jugadoractual==1:
+            self.panel2.setEnabled(True)
+        elif self.ogl.jugadoractual==2:
+            self.panel3.setEnabled(True)
+        elif self.ogl.jugadoractual==3:
+            self.panel4.setEnabled(True)
         
     @QtCore.pyqtSlot()      
     def on_actionLenguaje_activated(self):
         fr=frmLanguage(self, "frmlanguage")
         fr.open()
+                    
+    def volver_a_tirar(self):
+        self.actionDado.setEnabled(True)
