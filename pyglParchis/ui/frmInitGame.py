73c9a8b67522f61890a786ca680525505a11a5d4
turulomio <turulomio@ecefa015-bacb-442a-bd82-e7c1c7b1ecc8>
2011-10-15 19:56:00 +0000

    Añadido ui eleccion primer jugador

--- pyglParchis/ui/frmInitGame.py
+++ pyglParchis/ui/frmInitGame.py
@@ -3,8 +3,8 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 from Ui_frmInitGame import *
-from wdgQT import *
-class frmInitGame(QDialog, Ui_frmInitGame):
+
+class frmInitGame(QWizard, Ui_frmInitGame):
     def __init__(self, parent = None, name = None, modal = False):
         QDialog.__init__(self, parent)
         if name:
