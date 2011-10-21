1bf09df401d9aa73d4be7a1a25dad0f0e2a3bf5a
turulomio <turulomio@ecefa015-bacb-442a-bd82-e7c1c7b1ecc8>
2011-10-21 21:09:56 +0000

    Mejorando gui

--- pyglParchis/i18n/glparchis_ru.ts
+++ pyglParchis/i18n/glparchis_ru.ts
@@ -407,7 +407,7 @@ p, li { white-space: pre-wrap; }
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui/wdgUserPanel.ui" line="181"/>
+        <location filename="ui/wdgUserPanel.ui" line="270"/>
         <source>Muestra el historial</source>
         <translation type="unfinished"></translation>
     </message>
