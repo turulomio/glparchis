99db0d5a56e88053018a2538ab22f2a25b4b210d
turulomio <turulomio@ecefa015-bacb-442a-bd82-e7c1c7b1ecc8>
2012-09-03 06:56:53 +0000

    Añádida variable rootdir para cambiar directorio de instalacion

--- pyglParchis/glparchis.py
+++ pyglParchis/glparchis.py
@@ -2,7 +2,8 @@
 #-*- coding: utf-8 -*- 
 
 import sys, os
-sys.path.append("/usr/lib/glparchis")
+rootdir="/usr"
+sys.path.append(rootdir+"/lib/glparchis")
 os.chdir(os.path.expanduser("~/.glparchis/"))
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
@@ -22,7 +23,7 @@ app = QApplication(sys.argv)
 translator = QTranslator(app)
 locale=QLocale()
 a=locale.system()
-translator.load("/usr/share/glparchis/glparchis_" + a.name() + ".qm")
+translator.load(rootdir+"/share/glparchis/glparchis_" + a.name() + ".qm")
 app.installTranslator(translator);
 
 frmMain = frmMain() 
