a481e9c2da4e691c53575377c5162bbefbc73afd
turulomio <turulomio@ecefa015-bacb-442a-bd82-e7c1c7b1ecc8>
2012-09-12 06:55:43 +0000

    Ahora funciona limpiamente

--- distribution.sh
+++ distribution.sh
@@ -12,6 +12,10 @@ echo "  - pyopengl (ultima version)"
 echo "  - pywin32 (ultima version)"
 echo "  - Inno Setup (ultima version)"
 
+#Genera los Ui_ y resto
+make compile
+
+#Copia los fuentes
 mkdir $DIR
 mkdir $DIR/doc
 mkdir $DIR/i18n
@@ -59,49 +63,14 @@ cp 	sounds/*.ogg \
 cp 	saves/*.glparchis \
 	$DIR/saves
 
-
-
 ###### sources linux. Ui se ha compilado antes
 echo "  * Comprimiendo codigo fuente..."
 tar cvz  -f dist/glparchis-src-$VERSION.tar.gz $DIR/ -C $DIR > /dev/null
 chmod 666 dist/glparchis-src-$VERSION.tar.gz
 
-######## COMPILA
+######## INSTALA
 DESTDIR=$DIR/dist
-PREFIXBIN=$DESTDIR/bin
-PREFIXLIB=$DESTDIR/lib/glparchis
-PREFIXSHARE=$DESTDIR/share/glparchis 
-PREFIXSOUND=$DESTDIR/share/glparchis/sounds
-mkdir -p  $DESTDIR
-mkdir -p  $PREFIXBIN
-mkdir -p  $PREFIXLIB
-mkdir -p  $PREFIXSHARE
-mkdir -p  $PREFIXSOUND
-
-#pyuic4 $DIR/ui/frmAbout.ui > $PREFIXLIB/Ui_frmAbout.py
-#pyuic4 $DIR/ui/frmInitGame.ui > $PREFIXLIB/Ui_frmInitGame.py
-#pyuic4 $DIR/ui/frmMain.ui > $PREFIXLIB/Ui_frmMain.py
-#pyuic4 $DIR/ui/frmSettings.ui > $PREFIXLIB/Ui_frmSettings.py
-#pyuic4 $DIR/ui/frmShowCasilla.ui > $PREFIXLIB/Ui_frmShowCasilla.py
-#pyuic4 $DIR/ui/frmShowFicha.ui > $PREFIXLIB/Ui_frmShowFicha.py
-#pyuic4 $DIR/ui/wdgUserPanel.ui > $PREFIXLIB/Ui_wdgUserPanel.py
-#pyuic4 $DIR/ui/wdgGame.ui > $PREFIXLIB/Ui_wdgGame.py
-#pyrcc4 $DIR/images/glparchis.qrc > $PREFIXLIB/glparchis_rc.py
-#cd $DIR
-#pylupdate4 -noobsolete glparchis.pro
-#lrelease glparchis.pro
-#cd ..
-
-install $DIR/glparchis.py $PREFIXBIN/glparchis
-install $DIR/glparchis.bat $DESTDIR
-install $DIR/libglparchis.py $PREFIXLIB
-install $DIR/ui/*.py $PREFIXLIB
-install $DIR/i18n/*.qm $PREFIXSHARE
-install $DIR/doc/glparchis-* $PREFIXSHARE
-install $DIR/AUTHORS-EN.txt  AUTHORS-ES.txt  CHANGELOG-EN.txt  CHANGELOG-ES.txt  GPL-3.txt  INSTALL-EN.txt  INSTALL-ES.txt  RELEASES-EN.txt  RELEASES-ES.txt  $PREFIXSHARE
-install $DIR/sounds/*.ogg $PREFIXSOUND
-install $DIR/images/*.py $PREFIXLIB
-install $DIR/images/*.ico $PREFIXSHARE
+DESTDIR=$DESTDIR make all
 
 ###### install pyinstaller
 cd $DIR
@@ -111,9 +80,10 @@ cd ..
 
 
 ####### binaries linux
-mv $PREFIXBIN/glparchis $PREFIXBIN/glparchis.py
-python $DIR/pyinstaller-2.0/pyinstaller.py -o $DIR/pyinstallerlinux -i $DIR/dist/share/glparchis/ficharoja.ico -w -p $DIR/dist/lib/glparchis $DIR/dist/bin/glparchis.py
+mv $DESTDIR/bin/glparchis $DESTDIR/bin/glparchis.py
+python $DIR/pyinstaller-2.0/pyinstaller.py -o $DIR/pyinstallerlinux -i $DESTDIR/share/glparchis/ficharoja.ico -w -p $DESTDIR/lib/glparchis $DESTDIR/bin/glparchis.py
 cd $DIR/pyinstallerlinux/dist/
+echo "Execute glparchis and play" > README.txt
 echo "  * Comprimiendo binario linux..."
 tar cvz  -f ../../../dist/glparchis-linux-$VERSION.tar.gz * > /dev/null
 cd ../../../
@@ -122,9 +92,9 @@ echo "
 @echo off
 cd bin
 c:/Python27/python.exe glparchis.py" > $DESTDIR/glparchis.bat
-sed -i -e 's:WindowsVersion=False:WindowsVersion=True:' $PREFIXLIB/libglparchis.py
+sed -i -e 's:WindowsVersion=False:WindowsVersion=True:' $DESTDIR/lib/glparchis/libglparchis.py
 
-wine $HOME/.wine/drive_c/Python27/python.exe  $DIR/pyinstaller-2.0/pyinstaller.py -o $DIR/pyinstallerwindows -i $DIR/dist/share/glparchis/ficharoja.ico -w -p $DIR/dist/lib/glparchis $DIR/dist/bin/glparchis.py
+wine $HOME/.wine/drive_c/Python27/python.exe  $DIR/pyinstaller-2.0/pyinstaller.py -o $DIR/pyinstallerwindows -i $DESTDIR/share/glparchis/ficharoja.ico -w -p $DESTDIR/lib/glparchis $DESTDIR/bin/glparchis.py
 sed -i -e "s:XXXXXXXX:$VERSION:" glparchis.iss
 wine $HOME/.wine/drive_c/Program\ Files\ \(x86\)/Inno\ Setup\ 5/ISCC.exe /odist glparchis.iss
 sed -i -e "s:$VERSION:XXXXXXXX:" glparchis.iss #Deja en X la versión
