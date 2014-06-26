0de040b597c406e6e39c2fe698a84aa70a3b6e9b
turulomio <turulomio@ecefa015-bacb-442a-bd82-e7c1c7b1ecc8>
2014-06-26 04:22:26 +0000

    Now logs when enter a pawn or must open a blockade

--- pyglParchis/ui/frmHelp.py
+++ pyglParchis/ui/frmHelp.py
@@ -41,4 +41,33 @@ class frmHelp(QDialog, Ui_frmHelp):
             self.trUtf8("<h3>Vistas del tablero</h3>")+                 
             self.trUtf8("Se puede cambiar la vista del tablero pulsando la tecla 'm'.<p>")
         )
+"""
+Basics: Parcheesi is played with two dice and the goal of the game is to move each of your pawns home to the center space. The most popular Parcheesi boards in America have seventy-two spaces around the board, twelve of which are darkened safe spaces where a pawn cannot be captured.
 
+Each player selects four pawns of the same color and places them in their “nest” or starting area. The game board should be positioned so that each player's nest is to their right hand. Pawns enter play onto the darkened space toward the left of their nest and continue counter-clockwise around the board to the home path directly in front of the player.
+
+Each player rolls a die, highest roller goes first, play continues clockwise to the left. Each turn players throw both dice and use the values shown to move their pawns around the board. If an amount on one or both of the dice cannot be moved that amount is forfeited.
+
+Entering Pawns: Five has a special value in entering pawns out of the nest where they begin the game. A player may enter a pawn only by throwing a total of five on the dice or if either of the dice shows a five. Each time a five is tossed, the player must enter a pawn when possible.
+
+Capture: An opponent's pawn resting on a lighter, non-safe space can be captured by landing on the same space by the amount shown on either die. The captured pawn is sent back to its nest and the turn continues with playing of any additional values shown on the dice. Also, each time a player captures an opponent’s pawn that player is awarded twenty movement points that may be moved with any one pawn at the end of their turn. If the bonus movement amount cannot be used it is forfeited.
+
+An opponent’s pawn on a darker safe space is not capturable except when a pawn is entering onto that space from its nest. In this case enter the pawn as usual and the opponent’s pawn is captured.
+
+It is not possible to end a turn with a pawn resting on the same space as an opponent’s pawn, even on a safety space.
+
+Blockade: Two of a player's pawns resting on the same space can form a blockade preventing all players from passing, including the blockading player’s pawns. The player whose pawns are blocking the path may keep them together for three turns or until there is no other pawn for that player to move. After three turns of blockading, at least one of the pawns must be moved on the fourth turn so that the two pawns rest on different spaces at end of the turn. If a player's move can't get beyond the blockade he can go as close as he wants. And he can win the game if he wants too.
+
+Should a blockade occupy a player’s entry space, it will prevent that player from entering pawns into play.
+
+It is not possible for a player to rest more than two pawns on the same space.
+
+Doublet: When a doublet (doubles) is tossed, the player gains another roll of the dice. In addition, if all that player’s pawns are outside the nest, the values on reverse side of dice are also used. For example, a player who rolls 6-6 can also move 1-1 in any combination. Therefore, when a doublet is tossed, the player has a total of fourteen spaces to move one or more pawns.
+
+Third consecutive doublet rolled in one turn is a penalty and pawns are not moved amount shown on dice. Player with three doublet penalty also removes their pawn closest to home, back to their nest and turn ends.
+
+Home: The center home space can only be entered by exact throw of the die or dice. When a pawn enters the center space by exact count, that player is awarded ten movement points that may be moved with any one pawn still in play at the end of their turn. If the bonus movement amount cannot be used it is forfeit.
+
+Each player has their own home path in front of them and may not enter another’s, so when a pawn is on its home path it can no longer be captured.
+
+Winning: The first player to get all four pawns home wins, at which point the winner must yell "PARCHEESI"."""
