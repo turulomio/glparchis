2e61d5144919a495ad53a7a1edabf77f69bd7dc9
turulomio <turulomio@ecefa015-bacb-442a-bd82-e7c1c7b1ecc8>
2017-07-22 08:03:09 +0000

    Changig Makefile system

---
+++ pyglParchis/Makefile.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python3
+import argparse
+import datetime
+import os
+import sys
+sys.path.append("ui")
+import platform
+from subprocess import call, check_call
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import cpu_count
+from libglparchis import version
+
+def shell(*args):
+    print(" ".join(args))
+    call(args,shell=True)
+
+def build_dir():
+    pyversion="{}.{}".format(sys.version_info[0], sys.version_info[1])
+    if sys.platform=="win32":
+        so="win"
+        if platform.architecture()[0]=="64bit":
+            pl="amd64"
+        else:
+            pl="win32"
+            return "build/exe.{}-{}".format(sys.platform, pyversion)
+    else:#linux
+        so="linux"
+        if platform.architecture()[0]=="64bit":
+            pl="x86_64"
+        else:
+            pl="i686"
+    return "build/exe.{}-{}-{}".format(so, pl, pyversion)
+    
+def filename_output():
+    if sys.platform=="win32":
+        so="windows"
+        if platform.architecture()[0]=="64bit":
+            pl="amd64"
+        else:
+            pl="win32"
+    else:#linux
+        so="linux"
+        if platform.architecture()[0]=="64bit":
+            pl="x86_64"
+        else:
+            pl="x86"
+    return "xulpymoney-{}-{}.{}".format(so,  version, pl)
+    
+
+
+if __name__ == '__main__':
+    start=datetime.datetime.now()
+    parser=argparse.ArgumentParser(prog='Makefile.py', description='Makefile in python', epilog="Developed by Mariano Muñoz", formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--doc', help="Generate docs and i18n",action="store_true",default=False)
+    parser.add_argument('--compile', help="App compilation",action="store_true",default=False)
+    parser.add_argument('--compile_images', help="App images compilation",action="store_true",default=False)
+    parser.add_argument('--destdir', help="Directory to install",action="store",default="/")
+    parser.add_argument('--uninstall', help="Uninstall",action="store_true",default=False)
+    parser.add_argument('--dist_sources', help="Make a sources tar", action="store_true",default=False)
+    parser.add_argument('--dist_linux', help="Make a Linux binary distribution", action="store_true",default=False)
+    parser.add_argument('--dist_windows', help="Make a Windows binary distribution", action="store_true",default=False)
+    parser.add_argument('--python', help="Python path", action="store",default='/usr/bin/python3')
+    args=parser.parse_args()
+
+    prefixbin=args.destdir+"/usr/bin"
+    prefixlib=args.destdir+"/usr/lib/glparchis"
+    prefixshare=args.destdir+"/usr/share/glparchis"
+    prefixpixmaps=args.destdir+"/usr/share/pixmaps"
+    prefixapplications=args.destdir+"/usr/share/applications"
+    prefixman=args.destdir+"/usr/share/man"
+    prefixsounds=args.destdir+"/usr/share/glparchis/sounds"
+
+    if "--dist_windows" in sys.argv and platform.system()!="Windows":
+        print("You need to be in Windows to pass this parameters")
+        sys.exit(1)
+    elif "--dist_windows" not in sys.argv and platform.system=="Windows":#In windows only dist_windows
+        print("You need to be in Linux to pass this parameters")
+        sys.exit(1)
+
+
+    if args.doc==True:
+        shell("pylupdate5 -noobsolete -verbose glparchis.pro")
+        shell("lrelease -qt5 glparchis.pro")
+    elif args.uninstall==True:
+        shell("rm " + prefixbin + "/glparchis*")
+        shell("rm -Rf " + prefixlib)
+        shell("rm -Rf " + prefixshare)
+        shell("rm -fr " + prefixpixmaps + "/glparchis.png")
+        shell("rm -fr " + prefixapplications +"/glparchis.desktop")
+    elif args.dist_sources==True:
+        shell("{} setup.py sdist".format(args.python))
+    elif args.dist_linux==True:
+        shell("{} setup.py build_exe".format(args.python))    
+        print (build_dir(), filename_output(), os.getcwd())
+        pwd=os.getcwd()
+        os.chdir(build_dir())
+        print (build_dir(), filename_output(), os.getcwd())
+        os.system("tar cvz -f '{0}/dist/{1}.tar.gz' * -C '{0}/{2}/'".format(pwd, filename_output(),  build_dir()))
+    elif args.dist_windows==True:
+        check_call([sys.executable, "setup.py","bdist_msi"])
+    elif args.compile==True:
+        futures=[]
+        with ProcessPoolExecutor(max_workers=cpu_count()+1) as executor:
+            futures.append(executor.submit(shell, "pyuic5 ui/frmAbout.ui -o ui/Ui_frmAbout.py"))
+            futures.append(executor.submit(shell, "pyuic5 ui/frmHelp.ui -o ui/Ui_frmHelp.py"))
+            futures.append(executor.submit(shell, "pyuic5 ui/frmInitGame.ui -o ui/Ui_frmInitGame.py"))
+            futures.append(executor.submit(shell, "pyuic5 ui/frmMain.ui -o ui/Ui_frmMain.py"))
+            futures.append(executor.submit(shell, "pyuic5 ui/frmSettings.ui -o ui/Ui_frmSettings.py"))
+            futures.append(executor.submit(shell, "pyuic5 ui/frmShowCasilla.ui -o ui/Ui_frmShowCasilla.py"))
+            futures.append(executor.submit(shell, "pyuic5 ui/frmShowFicha.ui -o ui/Ui_frmShowFicha.py"))
+            futures.append(executor.submit(shell, "pyuic5 ui/wdgPlayerDado.ui -o ui/Ui_wdgPlayerDado.py"))
+            futures.append(executor.submit(shell, "pyuic5 ui/wdgPlayer.ui -o ui/Ui_wdgPlayer.py"))
+            futures.append(executor.submit(shell, "pyuic5 ui/wdgUserPanel.ui -o ui/Ui_wdgUserPanel.py"))
+            futures.append(executor.submit(shell, "pyuic5 ui/wdgGame.ui -o ui/Ui_wdgGame.py"))
+    elif args.compile_images==True:
+            shell("pyrcc5 images/glparchis.qrc -o images/glparchis_rc.py")
+    else:
+        shell("install -o root -d "+ prefixbin)
+        shell("install -o root -d "+ prefixlib)
+        shell("install -o root -d "+ prefixshare)
+        shell("install -o root -d "+ prefixpixmaps)
+        shell("install -o root -d "+ prefixapplications)
+        shell("install -o root -d "+ prefixsounds)
+        shell("install -o root -d "+ prefixman+"/man1")
+        shell("install -o root -d "+ prefixman+"/es/man1")
+        shell("install -o root -d "+ prefixman+"/fr/man1")
+        shell("install -o root -d "+ prefixman+"/ro/man1")
+        shell("install -o root -d "+ prefixman+"/ru/man1")
+
+        shell("install -m 755 -o root glparchis.py "+ prefixbin+"/glparchis")
+        shell("install -m 644 -o root ui/*.py libglparchis.py images/*.py "+ prefixlib)
+        shell("install -m 644 -o root i18n/*.qm " + prefixlib)
+        shell("install -m 644 -o root glparchis.desktop "+ prefixapplications)
+        shell("install -m 644 -o root images/ficharoja.png "+ prefixshare)
+        shell("install -m 644 -o root sounds/* "+ prefixsounds)
+        shell("install -m 644 -o root images/ficharoja.png "+ prefixpixmaps + "/glparchis.png")
+        shell("install -m 644 -o root GPL-3.txt AUTHORS-EN.txt AUTHORS-ES.txt CHANGELOG-EN.txt CHANGELOG-ES.txt INSTALL-EN.txt INSTALL-ES.txt RELEASES.txt"+ prefixshare)
+        
+    print ("*** Process took {} using {} processors ***".format(datetime.datetime.now()-start , cpu_count()))
+
--- pyglParchis/setup.py
+++ pyglParchis/setup.py
@@ -1,114 +1,195 @@
+##from cx_Freeze import setup, Executable
+##import os
+##import shutil
+##import subprocess
+##import sys
+##import platform 
+##import PyQt5
+##sys.path.append('ui')
+##sys.path.append('images')
+##from libglparchis import version
+##
+##def winversion():
+##    lastpoint="0"
+##    if version.find("+")!=-1:
+##        lastpoint="1"
+##        
+##    versio=version.replace("+","")
+##    return versio[:-4]+"."+versio[4:-2]+"."+versio[6:]+"."+lastpoint
+##    
+##def build_dir():
+##    pyversion="{}.{}".format(sys.version_info[0], sys.version_info[1])
+##    if sys.platform=="win32":
+##        so="win"
+##        if platform.architecture()[0]=="64bit":
+##            pl="amd64"
+##        else:
+##            pl="win32"
+##            return "build/exe.{}-{}".format(sys.platform, pyversion)
+##    else:#linux
+##        so="linux"
+##        if platform.architecture()[0]=="64bit":
+##            pl="x86_64"
+##        else:
+##            pl="i686"
+##    return "build/exe.{}-{}-{}".format(so, pl, pyversion)
+##    
+##def filename_output():
+##    if sys.platform=="win32":
+##        so="windows"
+##        if platform.architecture()[0]=="64bit":
+##            pl="amd64"
+##        else:
+##            pl="win32"
+##    else:#linux
+##        so="linux"
+##        if platform.architecture()[0]=="64bit":
+##            pl="x86_64"
+##        else:
+##            pl="x86"
+##    return "glparchis-{}-{}.{}".format(so,  version, pl)
+##
+##print ("Building for", sys.platform, version, winversion())
+##name="glParchis"
+##
+###Add files for all platforms
+##include_files=['sounds/', 'images/ficharoja.ico', 'GPL-3.txt']
+##include_files.append(('i18n', 'i18n')) #include files puede ser tambien una tupla (origen,destino)
+##
+###Build options
+##if sys.platform=='win32':
+##        shutil.rmtree("build/", ignore_errors=True)
+##        base = 'Win32GUI'     
+##        include_files.append((PyQt5.__path__[0]+'/plugins/audio/qtaudio_windows.dll', 'audio/qtaudio_windows.dll' ))#Si no no sonaba en windows solo
+##        include_files.append('glparchis.iss')
+##        build_exe_options = dict(
+##           create_shared_zip=False,
+##           includes = ['OpenGL','OpenGL.platform.win32','OpenGL.arrays','OpenGL.arrays.ctypesarrays', 'OpenGL.arrays.lists','OpenGL.converters','PyQt5.QtNetwork','PyQt5.QtWebKit','PyQt5.QtPrintSupport'],
+##           excludes=[], 
+##           include_files=include_files
+##           )
+##
+##        options={
+##      #'bdist_msi': build_msi_options,
+##               'build_exe': build_exe_options
+##               }
+##               
+##               
+##               
+##else:#linux
+##        base="Console"
+##        build_options = dict(
+##           includes = ['OpenGL','OpenGL.platform.glx','OpenGL.arrays','OpenGL.arrays.ctypesarrays', 'OpenGL.arrays.lists','OpenGL.converters','PyQt5.QtNetwork','PyQt5.QtWebKit','PyQt5.QtPrintSupport'],
+##           excludes = [], 
+##           include_files=include_files
+##           )
+##        options=dict(build_exe = build_options)
+##
+##executables = [
+##      Executable('glparchis.py', base=base, icon='images/ficharoja.ico', shortcutName= name, shortcutDir='ProgramMenuFolder')
+##]
+##
+##setup(name=name,
+##      version = winversion(),
+##      author = 'Mariano Muñoz',
+##      description = 'Parcheesi Game',
+##      options = options,
+##      executables = executables)
+##
+##
+###Post setup
+##if sys.platform=="win32":
+##    os.chdir(build_dir())
+##    
+##    inno="c:/Program Files (x86)/Inno Setup 5/ISCC.exe"
+##    if platform.architecture()[0]=="32bit":
+##        inno=inno.replace(" (x86)", "")
+##    
+##    subprocess.call([inno,  "/o../",  "/DVERSION_NAME={}".format(winversion()), "/DFILENAME={}".format(filename_output()),"glparchis.iss"], stdout=sys.stdout)
+##else:   #Linux
+##    print (build_dir(), filename_output(), os.getcwd())
+##    pwd=os.getcwd()
+##    os.chdir(build_dir())
+##    print (build_dir(), filename_output(), os.getcwd())
+##    os.system("tar cvz -f '{0}/build/{1}.tar.gz' * -C '{0}/{2}/'".format(pwd, filename_output(),  build_dir()))
+##    
+##    
+    
 from cx_Freeze import setup, Executable
-import os
-import shutil
-import subprocess
 import sys
-import platform
-import PyQt5
 sys.path.append('ui')
 sys.path.append('images')
 from libglparchis import version
 
-def winversion():
-    lastpoint="0"
-    if version.find("+")!=-1:
-        lastpoint="1"
-        
-    versio=version.replace("+","")
-    return versio[:-4]+"."+versio[4:-2]+"."+versio[6:]+"."+lastpoint
-    
-def build_dir():
-    pyversion="{}.{}".format(sys.version_info[0], sys.version_info[1])
-    if sys.platform=="win32":
-        so="win"
-        if platform.architecture()[0]=="64bit":
-            pl="amd64"
-        else:
-            pl="win32"
-            return "build/exe.{}-{}".format(sys.platform, pyversion)
-    else:#linux
-        so="linux"
-        if platform.architecture()[0]=="64bit":
-            pl="x86_64"
-        else:
-            pl="i686"
-    return "build/exe.{}-{}-{}".format(so, pl, pyversion)
-    
-def filename_output():
-    if sys.platform=="win32":
-        so="windows"
-        if platform.architecture()[0]=="64bit":
-            pl="amd64"
-        else:
-            pl="win32"
-    else:#linux
-        so="linux"
-        if platform.architecture()[0]=="64bit":
-            pl="x86_64"
-        else:
-            pl="x86"
-    return "glparchis-{}-{}.{}".format(so,  version, pl)
+print ("Building for", sys.platform, version)
+name="glparchis"
 
-print ("Building for", sys.platform, version, winversion())
-name="glParchis"
-
-#Add files for all platforms
-include_files=['sounds/', 'images/ficharoja.ico', 'GPL-3.txt']
-include_files.append(('i18n', 'i18n')) #include files puede ser tambien una tupla (origen,destino)
+#Add files
+include_files=[ 'images/glparchis.ico', 'GPL-3.txt']
+include_files.append(("i18n/glparchis_es.qm", "i18n/glparchis_es.qm"))
+include_files.append(("i18n/glparchis_fr.qm", "i18n/glparchis_fr.qm"))
+include_files.append(("i18n/glparchis_ro.qm", "i18n/glparchis_ro.qm"))
+include_files.append(("i18n/glparchis_ru.qm", "i18n/glparchis_ru.qm"))
 
 #Build options
 if sys.platform=='win32':
-        shutil.rmtree("build/", ignore_errors=True)
-        base = 'Win32GUI'     
-        include_files.append((PyQt5.__path__[0]+'/plugins/audio/qtaudio_windows.dll', 'audio/qtaudio_windows.dll' ))#Si no no sonaba en windows solo
-        include_files.append('glparchis.iss')
-        build_exe_options = dict(
-           create_shared_zip=False,
-           includes = ['OpenGL','OpenGL.platform.win32','OpenGL.arrays','OpenGL.arrays.ctypesarrays', 'OpenGL.arrays.lists','OpenGL.converters','PyQt5.QtNetwork','PyQt5.QtWebKit','PyQt5.QtPrintSupport'],
-           excludes=[], 
-           include_files=include_files
-           )
+      #base = 'Win32GUI'
+      base="Console"
+      shortcut_table = [
+          ("DesktopShortcut",        # Shortcut
+           "DesktopFolder",          # Directory_
+           "glParchis",     # Name
+           "TARGETDIR",              # Component_
+           "[TARGETDIR]glparchis.exe",   # Target
+           None,                     # Arguments
+           None,                     # Description
+           None,                     # Hotkey
+           None,                     # Icon
+           None,                     # IconIndex
+           None,                     # ShowCmd
+           'TARGETDIR'               # WkDir
+           ),
 
-        options={
-      #'bdist_msi': build_msi_options,
-               'build_exe': build_exe_options
-               }
-               
-               
-               
+          ("StartupShortcut",        # Shortcut
+           "StartupFolder",          # Directory_
+           "glParchis",     # Name
+           "TARGETDIR",              # Component_
+           "[TARGETDIR]glparchis.exe",   # Target
+           None,                     # Arguments
+           None,                     # Description
+           None,                     # Hotkey
+           None,                     # Icon
+           None,                     # IconIndex
+           None,                     # ShowCmd
+           'TARGETDIR'               # WkDir
+           ),
+      ]
+
+      msi_data = {"Shortcut": shortcut_table}  # This will be part of the 'data' option of bdist_msi
+      build_msi_options = {
+           'upgrade_code': '{3849730B-2375-4F76-B4A5-A6677A23AB9B}',
+           'add_to_path': False,
+           'initial_target_dir': r'[ProgramFilesFolder]\%s' % (name),
+           'data': msi_data
+            }
+ 
+      build_exe_options = dict(includes = [],excludes=[], include_files=include_files)
+      options={'bdist_msi': build_msi_options, 'build_exe': build_exe_options}
+
 else:#linux
-        base="Console"
-        build_options = dict(
-           includes = ['OpenGL','OpenGL.platform.glx','OpenGL.arrays','OpenGL.arrays.ctypesarrays', 'OpenGL.arrays.lists','OpenGL.converters','PyQt5.QtNetwork','PyQt5.QtWebKit','PyQt5.QtPrintSupport'],
-           excludes = [], 
-           include_files=include_files
-           )
-        options=dict(build_exe = build_options)
+      base="Console"
+      build_options = dict(includes = [], excludes = [], include_files=include_files)
+      options=dict(build_exe = build_options)
 
 executables = [
       Executable('glparchis.py', base=base, icon='images/ficharoja.ico', shortcutName= name, shortcutDir='ProgramMenuFolder')
 ]
 
 setup(name=name,
-      version = winversion(),
+      version = version,
       author = 'Mariano Muñoz',
-      description = 'Parcheesi Game',
+      author_email="turulomio@yahoo.es", 
+      description = 'Search devices in my LAN',
       options = options,
+      url="https://sourceforge.net/projects/glparchis/", 
       executables = executables)
-
-
-#Post setup
-if sys.platform=="win32":
-    os.chdir(build_dir())
-    
-    inno="c:/Program Files (x86)/Inno Setup 5/ISCC.exe"
-    if platform.architecture()[0]=="32bit":
-        inno=inno.replace(" (x86)", "")
-    
-    subprocess.call([inno,  "/o../",  "/DVERSION_NAME={}".format(winversion()), "/DFILENAME={}".format(filename_output()),"glparchis.iss"], stdout=sys.stdout)
-else:   #Linux
-    print (build_dir(), filename_output(), os.getcwd())
-    pwd=os.getcwd()
-    os.chdir(build_dir())
-    print (build_dir(), filename_output(), os.getcwd())
-    os.system("tar cvz -f '{0}/build/{1}.tar.gz' * -C '{0}/{2}/'".format(pwd, filename_output(),  build_dir()))
